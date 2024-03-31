7150c62b38b24ff75e52c9e5e6c3dde6b678a08e
greggot <alexgerggot@gmail.com>
2024-03-31 17:45:42 +0300

    Clean up mesh shader

--- shaders/mesh_vertex.glsl
+++ shaders/mesh_vertex.glsl
@@ -1,10 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 pos;
-// layout (location = 1) in vec3 norm;
 
 uniform mat4 model;
-// uniform mat4 view;
-// uniform mat4 projection;
 
 varying vec3 normal;
 varying vec3 fragment_pos;
@@ -12,9 +9,4 @@ varying vec3 fragment_pos;
 void main()
 {
 	gl_Position = model * vec4(pos, 1.0);
-	// gl_Position = projection * view * model * vec4(pos, 1.0);
-	
-	// normal = norm;
-	// fragment_pos = vec3(model * vec4(pos, 1.0));
-	// fragment_pos = pos;
 }
